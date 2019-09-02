46f0ef523b9e6d774c3a3be0b109868a9d841b49
Mohamad Maarouf <mohamad.maarouf@myob.com>
2019-09-02 16:29:27 +1000

    Add ability to return a number given an string

---
+++ src/com/stringcalc/StringCalculator.java
@@ -0,0 +1,7 @@
+package com.stringcalc;
+
+public class StringCalculator {
+    public static int add(String s) {
+        return 0;
+    }
+}
--- test/com/stringcalc/StringCalculatorTest.java
+++ test/com/stringcalc/StringCalculatorTest.java
@@ -1,5 +1,16 @@
 package com.stringcalc;
 
-public class StringCalculatorTest{
+import org.junit.Test;
 
-        }
+import static junit.framework.TestCase.assertEquals;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void shouldReturnZeroWhenGivenAnEmptyString() {
+        int actual = StringCalculator.add("");
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+}
