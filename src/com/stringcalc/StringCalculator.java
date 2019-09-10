954ccc32fed3d6707a03c228067d5e954b51e62b
Eathan Langford <eathan.langford@myob.com>
2019-09-10 16:13:31 +1000

    add method to allow for ignoring numbers over 1000

--- src/com/stringcalc/StringCalculator.java
+++ src/com/stringcalc/StringCalculator.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class StringCalculator {
 
-    public static List<String> checkSupportedDelimiters(String stringInput) throws Exception {
+    public static List<String> checkSupportedDelimiters(String stringInput) {
         List<String> supportedDelimiters = new ArrayList<>();
         String semiColon = (stringInput.contains(";")?";":null);
         String newLine = (stringInput.contains("\n")?"\n":null);
@@ -17,7 +17,7 @@ public class StringCalculator {
         return supportedDelimiters;
     }
 
-    public static String extractStringFromDeclaredDelimiter(String stringInput, List<String> supportedDelimiters) throws Exception {
+    public static String extractStringFromDeclaredDelimiter(String stringInput, List<String> supportedDelimiters) {
         if (stringInput.startsWith("//")){
             String[] splitStringWithDelimiter = stringInput.split("\n");
             supportedDelimiters.add(splitStringWithDelimiter[0].replace("//", ""));
@@ -37,10 +37,10 @@ public class StringCalculator {
         return stringInput;
     }
 
-    public static void checkForNegativeNumbers (List<String> numbersExtractedWithCommas) throws Exception{
+    public static void checkForNegativeNumbers (List<String> numbersExtractedWithComma) throws Exception{
         ArrayList<String> negativeNumbers = new ArrayList<>();
         String exceptionMessage = "Negative numbers not allowed: ";
-        for (String number:numbersExtractedWithCommas
+        for (String number:numbersExtractedWithComma
         ) {
             if (number.startsWith("-")) {
                 negativeNumbers.add(number);
@@ -51,6 +51,16 @@ public class StringCalculator {
             System.out.println(exceptionMessage);
             throw new Exception(exceptionMessage);
         }
+    }
+
+    public static List<String> checkForNumbersGreaterThan1000 (List<String> numbersExtractedWithComma){
+        for (String number:numbersExtractedWithComma
+             ) {
+            if (Integer.parseInt(number)>=1000){
+                numbersExtractedWithComma.remove(number);
+            }
+        }
+        return numbersExtractedWithComma;
     }
 
     public static int add(String stringInput) throws Exception {
@@ -64,8 +74,11 @@ public class StringCalculator {
         stringInput = replaceDelimitersWithCommas(stringInput, supportedDelimiters);
         List<String> numbersExtractedWithComma = Arrays.asList(stringInput.split(","));
         checkForNegativeNumbers(numbersExtractedWithComma);
-
         return numbersExtractedWithComma.stream()
-                .map(Integer::parseInt).mapToInt(Integer::intValue).sum();
+                .map(Integer::parseInt)
+                .filter(num-> num<1000)
+                .mapToInt(Integer::intValue)
+                .sum();
+
     }
 }
